39f679a8877aff75c70d0d610740a30f95674862
Blessy <blessymoses17@gmail.com>
2023-02-09 22:30:35 +0800

    scala project

--- AlphaScala/src/main/scala/SayHola.scala
+++ AlphaScala/src/main/scala/SayHola.scala
@@ -1,5 +1,12 @@
+import org.apache.spark.sql.SparkSession
+
 object SayHola {
   def main(args: Array[String]): Unit = {
-    print("Finally")
+    print("Scala Spark")
+    val spark = SparkSession.builder().appName(name = "MySpark").config("spark.master", "local").getOrCreate()
+    println("Spark session is created")
+    val sampleSeq = Seq((1, "Row1"), (2, "Row2"))
+    val df = spark.createDataFrame(sampleSeq).toDF(colNames = "row_id", "row_name")
+    df.show()
   }
 }
